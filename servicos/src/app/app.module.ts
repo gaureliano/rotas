29e4eebc893c9728aa44ef89de621dbe6fc68729
gaureliano <gaabriel.aureliano@outlook.com>
2021-06-08 20:10:06 -0300

    servicos

---
+++ servicos/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
