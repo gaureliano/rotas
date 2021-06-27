50a8d6c397313947ab6041d33e7a086d23b7448f
gaureliano <gaabriel.aureliano@outlook.com>
2021-06-27 20:07:45 -0300

    rotas

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: 'curso/:id', component: CursoDetalheComponent},
+  {path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
